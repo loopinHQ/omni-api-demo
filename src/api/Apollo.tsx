878bc5579fd8a5d87e080d046b674e97666be240
JrobT <jack@trute.co.uk>
2023-08-30 15:05:22 +0100

    remove port

--- src/api/Apollo.tsx
+++ src/api/Apollo.tsx
@@ -2,7 +2,7 @@ import ApolloClient from "apollo-boost";
 
 const createApolloClient = () => {
   return new ApolloClient({
-    uri: "http://omniapi.letsloopin.com:8080/graphql",
+    uri: "http://omniapi.letsloopin.com/graphql",
     headers: {
       authorization: `Bearer ${process.env.REACT_APP_BEARER_TOKEN}`,
     },
