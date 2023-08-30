06a4d2d832ee7271c6b6621cd13726276d52153e
JrobT <jack@trute.co.uk>
2023-08-30 15:05:10 +0100

    use api

--- src/api/Apollo.tsx
+++ src/api/Apollo.tsx
@@ -2,7 +2,7 @@ import ApolloClient from "apollo-boost";
 
 const createApolloClient = () => {
   return new ApolloClient({
-    uri: "http://localhost:8080/graphql", // Change this to your actual GraphQL endpoint
+    uri: "http://omniapi.letsloopin.com:8080/graphql",
     headers: {
       authorization: `Bearer ${process.env.REACT_APP_BEARER_TOKEN}`,
     },
